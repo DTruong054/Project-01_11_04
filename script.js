cac2684adf11b6abb29006fd85cc6e53ee1169ab
Unknown <DTruong054@west-mec.org>
2018-09-07 14:10:02 -0700

    Finished all projects

--- script.js
+++ script.js
@@ -1,10 +1,98 @@
 /*  Project 01_11_04
 
-    Author: 
-    Date:   
+    Author: Daniel Truong
+    Date:   9.7.18
 
     Filename: script.js
 */
 
 "use strict";
+//Global Variables
+var httpRequest = false;
+var countrySel;
+
+//This creates a new XML object
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest
+    } catch (requestError) {
+        document.getElementById("csset").style.visibility = "visible";
+        var zip = document.getElementById("zip").value;
+        if (zip.addEventListener) {
+            zip.removeEventListener("keyup", checkInput, false);
+        } else if(zip.attachEvent){
+            zip.attachEvent("onkeyup", checkInput);
+        }
+        return false;
+    }
+    return httpRequest;
+}
+
+//Implement radio buttons
+function checkButtons() {
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+    if (germany.checked || us.checked) {
+        document.getElementById("zipset").style.visibility = "visible";
+        if (germany.checked) {
+            countrySel = "de";
+        } else {
+            countrySel = "us";
+        }
+    }
+}
+
+//Test input with function checkInput
+function checkInput() {
+    var zip = document.getElementById("zip").value;
+    if (zip.length === 5) {
+        getLocation()
+    } else {
+        document.getElementById("city").value = "";
+        document.getElementById("state").value = "";
+    }
+}
+
+function getLocation() {
+    var zip = document.getElementById("zip").value;
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    //Createing AJAX request
+    httpRequest.abort();
+    httpRequest.open("get", "http://api.zippopotam.us/" + countrySel + "/" + zip, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+}
+
+//Input data for postal codes
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var resultData = JSON.parse(httpRequest.responseText);
+        var city = document.getElementById("city");
+        var state = document.getElementById("state");
+        city.value = resultData.places[0]["place name"];
+        state.value = resultData.places[0]["state abbreviation"];
+        document.getElementById("zip").blur();
+        document.getElementById("csset").style.visibility = "visible";
+    }
+}
+
+//Event listeners
+var germany = document.getElementById("germany");
+var us = document.getElementById("us");
+if (us.addEventListener) {
+    germany.addEventListener("click", checkButtons, false);
+    us.addEventListener("click", checkButtons, false)
+} else if(us.attachEvent){
+    germany.attachEvent("onclick", checkButtons);
+    us.attachEvent("onclick", checkButtons);
+}
+
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false);
+} else if (zip.attachEvent){
+    zip.attachEvent("onkeyup", checkInput);
+}
 
